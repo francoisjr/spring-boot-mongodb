5194b56a06a3a7ef295ec015b6536768381c9c51
Francis <francisdsj@gmail.com>
2021-01-20 17:18:40 -0300

    Obtendo post por id

---
+++ src/main/java/com/ffjunior/springbootmongo/resources/PostResource.java
@@ -0,0 +1,30 @@
+package com.ffjunior.springbootmongo.resources;
+
+import com.ffjunior.springbootmongo.domain.Post;
+import com.ffjunior.springbootmongo.domain.User;
+import com.ffjunior.springbootmongo.dto.UserDTO;
+import com.ffjunior.springbootmongo.services.PostService;
+import com.ffjunior.springbootmongo.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.net.URI;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@RestController
+@RequestMapping(value = "/posts")
+public class PostResource {
+
+    @Autowired
+    private PostService postService;
+
+
+    @RequestMapping(value = "/{id}", method = RequestMethod.GET)
+    public ResponseEntity<Post> findById(@PathVariable String id){
+        Post obj = postService.findById(id);
+        return ResponseEntity.ok().body(obj);
+    }
+}
---
+++ src/main/java/com/ffjunior/springbootmongo/services/PostService.java
@@ -0,0 +1,25 @@
+package com.ffjunior.springbootmongo.services;
+
+import com.ffjunior.springbootmongo.domain.Post;
+import com.ffjunior.springbootmongo.domain.User;
+import com.ffjunior.springbootmongo.dto.UserDTO;
+import com.ffjunior.springbootmongo.repository.PostRepository;
+import com.ffjunior.springbootmongo.repository.UserRepository;
+import com.ffjunior.springbootmongo.services.exception.ObjectNotFoundException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class PostService {
+
+    @Autowired
+    PostRepository postRepository;
+
+    public Post findById(String id){
+        Optional<Post> post = postRepository.findById(id);
+        return post.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+    }
+}
